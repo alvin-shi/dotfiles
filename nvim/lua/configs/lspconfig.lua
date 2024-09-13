5335c6e96f41896ee6f85dea26c450ea348f96e4
Alvin Shi <alvinzyshi@gmail.com>
2024-09-13 13:15:17 +1000

    fix lsp configs

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,14 +1,26 @@
 local configs = require("nvchad.configs.lspconfig")
+local on_init = configs.on_init
 local on_attach = configs.on_attach
 local capabilities = configs.capabilities
 
 local lspconfig = require "lspconfig"
+-- TODO: clean up config by following
+-- https://nvchad.com/docs/recipes
 
 lspconfig.yamlls.setup {
+  on_init = on_init,
   on_attach = on_attach,
   capabilities = capabilities,
 }
 
-lspconfig.ts_ls.setup{}
+lspconfig.ts_ls.setup{
+  on_init = on_init,
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
 
-lspconfig.ruby_lsp.setup{}
+lspconfig.ruby_lsp.setup{
+  on_init = on_init,
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
