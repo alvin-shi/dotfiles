57ded5d65f0cf6194dc941aa6b873f46ac3bca4a
Alvin Shi <alvinzyshi@gmail.com>
2024-11-04 15:08:57 +1100

    linting

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,4 +1,4 @@
-local configs = require("nvchad.configs.lspconfig")
+local configs = require "nvchad.configs.lspconfig"
 local on_init = configs.on_init
 local on_attach = configs.on_attach
 local capabilities = configs.capabilities
@@ -13,13 +13,19 @@ lspconfig.yamlls.setup {
   capabilities = capabilities,
 }
 
-lspconfig.ts_ls.setup{
+lspconfig.ts_ls.setup {
   on_init = on_init,
   on_attach = on_attach,
   capabilities = capabilities,
 }
 
-lspconfig.ruby_lsp.setup{
+lspconfig.ruby_lsp.setup {
+  on_init = on_init,
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+lspconfig.bashls.setup {
   on_init = on_init,
   on_attach = on_attach,
   capabilities = capabilities,
