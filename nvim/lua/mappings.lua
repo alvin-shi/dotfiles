15c1622ec835929ea1f245dc6ce4c594c61ec479
Alvin Shi <alvinzyshi@gmail.com>
2025-11-01 01:19:27 +1100

    remove empty line because of existing shortcut

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -5,8 +5,6 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("n", "oo", "o<ESC>", { desc = "Add empty line below without leaving normal mode" })
-map("n", "OO", "O<ESC>", { desc = "Add empty line above without leaving normal mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
