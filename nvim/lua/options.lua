d9ce37dae47ff61a77d73bab51c4259c3dcd2825
Alvin Shi <alvinzyshi@gmail.com>
2026-01-15 02:30:05 +1100

    set relative line number and add empty character symbol

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -6,3 +6,6 @@ require("luasnip").filetype_extend("typescript", { "javascript" })
 -- editor
 local set = vim.opt
 set.cc = { "81", "121" }
+set.relativenumber = true
+set.list = true
+set.listchars = { space = "·", trail = "·", tab = "  ↦" }
