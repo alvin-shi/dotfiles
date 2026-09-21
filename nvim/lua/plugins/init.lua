919ab02845670e0994e2591e393e247868ff39e5
Alvin Shi <alvinzyshi@gmail.com>
2026-09-22 04:31:16 +1000

    fix missing comma

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -24,7 +24,7 @@ return {
     opts = { ensure_installed = { "ruby", "bash" } },
   },
   {
-    url = "https://codeberg.org/andyg/leap.nvim"
+    url = "https://codeberg.org/andyg/leap.nvim",
     lazy = false,
     opts = {},
   },
