5064592425cb44b64ee76421bcc2c4b895440f24
Alvin Shi <alvinzyshi@gmail.com>
2026-09-22 04:30:09 +1000

    add kj as escape as well

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -6,6 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("i", "kj", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 map({ "n", "x", "o" }, "s", "<Plug>(leap)")
